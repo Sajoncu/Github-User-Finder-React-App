9e7e0625e07f223ad678022d8673774df74681ce
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-06-29 01:04:17 +0600

    class based component has been done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import './App.css';
+import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem';
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Github Finder</h1>
+        <Navbar title="GitHubFinder" icon="fab fa-github"/>
+        <UserItem />
       </div>
     )
   }
