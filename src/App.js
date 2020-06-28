ee961150249d02c30fbd90f6f61d5a98adbe2154
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-06-29 02:47:01 +0600

    fetch github user by calling axios with function based component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import './App.css';
 import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users'
+import axios from 'axios'
 class App extends Component {
-  render() {
+  state = {
+    users : [],
+    loading : false
+  };
+
+  async componentDidMount() {
+    this.setState({loading:true});
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({users:res.data, loading: false});
+  }
+
+  render(){
     return (
       <div>
         <Navbar title="GitHubFinder" icon="fab fa-github"/>
-        <UserItem />
+          <div className='container'>
+            <Users loading={this.state.loading} users={this.state.users}/>
+          </div>
       </div>
-    )
+    );
   }
 }
 
